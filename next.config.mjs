8e106afb6adc519a0e21b3dece4dbdb982b85919
Alejandro Jackson <alejack312@gmail.com>
2023-07-04 16:12:27 -0400

    Changing build stuff

--- next.config.mjs
+++ next.config.mjs
@@ -33,6 +33,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 };
 
 export default config;
