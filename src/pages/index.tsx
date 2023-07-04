79e6769e77650b1ea3e5394b124b1ce52a9dac4c
Alejandro Jackson <alejack312@gmail.com>
2023-07-04 05:09:23 -0400

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import {toast} from "react-hot-toast";
 
+
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -117,10 +118,14 @@ const PostView = (props: PostWithUser) => {
         />
         <div className="flex flex-col">
           <div className="flex gap-1 text-slate-300">
-            <span> {`@${author.username}`} </span> 
-            <span className="font-thin"> 
-              {` · ${dayjs(post.createdAt).fromNow()}`} 
-            </span>
+            <Link href={`/@${author.username}`}> 
+              <span> {`@${author.username}`} </span> 
+            </Link>
+            <Link href={`/post/${post.id}`}>
+              <span className="font-thin"> 
+                {` · ${dayjs(post.createdAt).fromNow()}`} 
+              </span>
+            </Link>
           </div>
             <span className="text-l">
               {post.content}
