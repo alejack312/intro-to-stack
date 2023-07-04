7a164b1762f51a97761d871797a6b5430f59a675
Alejandro Jackson <alejack312@gmail.com>
2023-07-04 14:30:00 -0400

    Profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,16 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
-
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>The Pocket</title>
+        <meta name="description" content="We could all use a little time outside of The Pocket…" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster
         position="bottom-center" 
       />
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,5 @@
 
-import type { User } from "@clerk/nextjs/dist/types/api";
+
 import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
 
@@ -8,16 +8,10 @@ import { RouterOutputs } from "~/utils/api";
 import { TRPCError } from "@trpc/server";
 
 
-const filterUserForClient = (user: User) => {
-    return {
-        id: user.id, 
-        username: user.username, 
-        profileImageUrl: user.profileImageUrl,
-    };
-};
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
